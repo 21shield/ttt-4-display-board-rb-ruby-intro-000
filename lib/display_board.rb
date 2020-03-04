6ef21db83c8c43da6e935c9557d210128419ecc2
Netaly Ramirez <21netaly@gmail.com>
2020-03-04 03:40:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,14 @@
 # out the current state.
 board =  [' ',' ',' ',' ',' ',' ' ,' ',' ',' ']
 def display_board(board)
-  row = '   '|'   '|'   '
+  row1 = " #{board[0]} "|" #{board[1]} "|" #{board[2]} "
   sep = "-----------"
-  puts row
+  row2 = " #{board[3]} "|" #{board[4]} "|" #{board[5]} "
+  row3 = " #{board[6]} "|" #{board[7]} "|" #{board[8]} "
+  puts row1
   puts sep
-  puts row
+  puts row2
   puts sep
-  puts row
+  puts row3
 
 end
