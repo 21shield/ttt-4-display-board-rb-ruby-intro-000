9b2d7b616a06993471e50a506ce9f0f0d2efd67c
Netaly Ramirez <21netaly@gmail.com>
2020-03-04 03:35:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board =  [' ',' ',' ',' ',' ',' ' ,' ',' ',' ']
+def display_board(board)
+  row = '   '|'   '|'   '
+  sep = "-----------"
+  puts row
+  puts sep
+  puts row
+  puts sep
+  puts row
+
+end
